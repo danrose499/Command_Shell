b10f4824e0a7e4e8678a7755f11d2e4cbbd618f6
vperls000 <53792766+vperls000@users.noreply.github.com>
2021-04-14 13:33:58 -0400

    Started on the clear and ls portion of list

--- list.c
+++ list.c
@@ -1,1 +1,41 @@
+//Viviana Perls
+//CSC 332
+//Group A: list
+//28 April 2021
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
+int main(int argc, char *argv[]){
+    int clearpid, lspid;
+    int clearstatus = 0;
+    int lsstatus = 0;
+
+    clearpid = fork();
+
+    if(clearpid == 0){
+        //inside child process
+        execlp("clear", "clear", NULL);
+        return 0;
+    }  
+
+    while(!WEXITSTATUS(clearstatus)){
+        waitpid(clearpid, &clearstatus, 0);
+    }
+    printf("exited clear process normally\n");
+
+    
+    lspid = fork();
+
+    if(lspid == 0){
+        //inside the child process
+        execlp("ls", "ls", "-l", NULL);
+        return 0;
+    }
+
+    while(!WEXITSTATUS(lsstatus)){
+        waitpid(lspid, &lsstatus, 0);
+    }
+
+    return 0;
+}

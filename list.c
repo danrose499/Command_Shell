358e188410187587a730014ee8a53bab4b0bc935
vperls000 <53792766+vperls000@users.noreply.github.com>
2021-04-19 18:05:53 -0400

    I think this is done, feel free to try it out with the other commands

--- list.c
+++ list.c
@@ -5,37 +5,66 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <errno.h>
+#include <fcntl.h>
+
+struct Pipe{
+    int read;
+    int write;
+};
 
 int main(int argc, char *argv[]){
-    int clearpid, lspid;
-    int clearstatus = 0;
-    int lsstatus = 0;
+    int clearpid;
+    int clearstatus = 1;
+    struct Pipe pipeline;
 
     clearpid = fork();
 
     if(clearpid == 0){
         //inside child process
         execlp("clear", "clear", NULL);
-        return 0;
+        return 1;
     }  
 
-    while(!WEXITSTATUS(clearstatus)){
+    while(!WIFEXITED(clearstatus)){
         waitpid(clearpid, &clearstatus, 0);
     }
-    printf("exited clear process normally\n");
 
     
-    lspid = fork();
+    pipe(&pipeline.read);
+    int lspid = fork();
+    int lsstatus = 1;
 
     if(lspid == 0){
         //inside the child process
+        close(pipeline.read);
+        dup2(pipeline.write,1);
         execlp("ls", "ls", "-l", NULL);
-        return 0;
+        return 1;
     }
 
-    while(!WEXITSTATUS(lsstatus)){
+    while(!WIFEXITED(lsstatus)){
         waitpid(lspid, &lsstatus, 0);
     }
+
+    int teepid = fork();
+    int teestatus = 1;
+    if(teepid == 0){
+        close(pipeline.write);
+        dup2(pipeline.read,0);
+        execlp("tee", "tee", "t1.txt", NULL);
+        perror("tee");
+        return 1;
+    }
+
+    close(pipeline.read);
+    close(pipeline.write);
+
+    while(!WIFEXITED(teestatus)){
+        waitpid(teepid, &teestatus, 0);
+    }
+
+    rename("t1.txt", "tree.txt");
 
     return 0;
 }

e214362b47fe47340807958ee5542584a6d9b0ea
Daniel Rosenthal <57165307+danrose499@users.noreply.github.com>
2021-04-23 16:05:01 -0400

    Got rid of extra header, cleaned up documentation

--- exit.c
+++ exit.c
@@ -6,37 +6,38 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
-#include <stdlib.h>
+
 #define len 1024
 
-char c1[len] = "", c2[len] = "", c3[len] = "", c4[len] = ""; // Four strings to hold the four most recent commands
+char c1[len] = "", c2[len] = "", c3[len] = "", c4[len] = ""; // Four c strings to hold the four most recent commands
 void logCommand(char newCommand[]);                          // Adds any command to the log after being called in shell
 void printCommands();                                        // Prints a list the last 4 commands to the terminal
 void lsDir();                                                // Prints a detailed list of all content of the current directory (like ls -l)
+void waitForReturn();                                        // Waits for "return" input before returning
 char returnCheck = '0';                                      // "return" key terminates shell
 
-
-
-int Exit() {
+int Exit() {                                                 // Main function to be called be shell
     printCommands();
     lsDir();
-    while(returnCheck != 0x0A){     // 0x0A is ASCII for "return" key
-        returnCheck = fgetc(stdin); // Get new user input until input is "return" key
-    }
-    return 0;                       // After "return" key, return 0 to shell to terminate
+    waitForReturn();
+    return 0;                                                // After "return" key is entered, return 0 to shell to terminate
 }
 
-void logCommand(char newCommand[]){
-    strcpy(c1, c2);
-    strcpy(c2, c3);
-    strcpy(c3, c4);
-    strcpy(c4, newCommand);
+void logCommand(char newCommand[]){                          // Function:
+    strcpy(c1, c2); strcpy(c2, c3); strcpy(c3, c4);          // Pushes each entry down the list, overwritting the oldest entry
+    strcpy(c4, newCommand);                                  // Adds newest command to the end of the list
 }
-void printCommands(){
-    printf("%s\n%s\n%s\n%s\n", c4, c3, c2, c1);
+void printCommands(){                                        // Function: Prints a list the last 4 commands to the terminal 
+    printf("\nPrevious four commands:\n");
+    printf("%s\n%s\n%s\n%s\n\n", c4, c3, c2, c1);
 }
-void lsDir(){
+void lsDir(){                                                // Function: Prints a detailed list of all content of the current directory (like ls -l)
         char *argv[] = {"ls", "-l", 0};
         execvp(argv[0], argv);
-        fprintf(stderr, "child ls -l failed!\n"); // Only prints if execvp fails
+        fprintf(stderr, "child ls -l failed!\n");            // Only prints if execvp fails
+}
+void waitForReturn(){                                        // Function: Waits for "return" input before returning
+    while(returnCheck != 0x0A){                              // 0x0A is ASCII for "return" key
+        returnCheck = fgetc(stdin);                          // Get new user input until input is "return" key
+    }
 }

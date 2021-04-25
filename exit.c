83a0acb7b42212df64abc98b42c4e54cbe8bb4c4
Daniel Rosenthal <57165307+danrose499@users.noreply.github.com>
2021-04-25 15:16:34 -0400

    Fixed spacing, edited  documentation

--- exit.c
+++ exit.c
@@ -7,34 +7,34 @@
 #include <unistd.h>
 #include <string.h>
 
-#define len 1024
+const int len = 1024;                                        // Default value set for length of c strings declared below 
+char c1[len] = "", c2[len] = "", c3[len] = "", c4[len] = ""; // Four c strings to hold the four most recent commands; Values are updated in logCommand() and printed in printCommands()
+char returnCheck = '0';                                      // "return" key terminates shell with; Used in waitForReturn() function
 
-char c1[len] = "", c2[len] = "", c3[len] = "", c4[len] = ""; // Four c strings to hold the four most recent commands
-void logCommand(char newCommand[]);                          // Adds any command to the log after being called in shell
+void logCommand(char newCommand[]);                          // Called in shell.c after shell.c executes a command adding that command to the command log
 void printCommands();                                        // Prints a list the last 4 commands to the terminal
 void lsDir();                                                // Prints a detailed list of all content of the current directory (like ls -l)
 void waitForReturn();                                        // Waits for "return" input before returning
-char returnCheck = '0';                                      // "return" key terminates shell
 
-int Exit() {                                                 // Main function to be called be shell
+int Exit() {                                                 // Exit function to be called by shell.c
     printCommands();
     lsDir();
     waitForReturn();
     return 0;                                                // After "return" key is entered, return 0 to shell to terminate
 }
 
-void logCommand(char newCommand[]){                          // Function:
-    strcpy(c1, c2); strcpy(c2, c3); strcpy(c3, c4);          // Pushes each entry down the list, overwritting the oldest entry
-    strcpy(c4, newCommand);                                  // Adds newest command to the end of the list
+void logCommand(char newCommand[]){                          // Function: Called in shell.c after shell.c executes a command adding that command to the command log
+    strcpy(c1, c2); strcpy(c2, c3); strcpy(c3, c4);          // Pushes each entry down the list, overwritting the oldest entry and freeing up c4...
+    strcpy(c4, newCommand);                                  // ... and saves the newest command into c4
 }
-void printCommands(){                                        // Function: Prints a list the last 4 commands to the terminal 
+void printCommands(){                                        // Function: Prints a list the last 4 commands called in shell.c to the terminal 
     printf("\nPrevious four commands:\n");
     printf("%s\n%s\n%s\n%s\n\n", c4, c3, c2, c1);
 }
 void lsDir(){                                                // Function: Prints a detailed list of all content of the current directory (like ls -l)
-        char *argv[] = {"ls", "-l", 0};
-        execvp(argv[0], argv);
-        fprintf(stderr, "child ls -l failed!\n");            // Only prints if execvp fails
+    char *argv[] = {"ls", "-l", 0};
+    execvp(argv[0], argv);
+    fprintf(stderr, "child ls -l failed!\n");            // Only prints if execvp fails
 }
 void waitForReturn(){                                        // Function: Waits for "return" input before returning
     while(returnCheck != 0x0A){                              // 0x0A is ASCII for "return" key

58d6247a7bf82b5863c34147903b1dc3aba0e710
Yasiris Ortiz <yasirisortiz24@gmail.com>
2021-04-26 18:06:21 -0400

    shell code

--- shell.c
+++ shell.c
@@ -1,4 +1,98 @@
-#include "tree.c"
-#include "list.c"
-#include "path.c"
-#include "exit.c"
+// #include "tree.c"
+// #include "list.c"
+// #include "path.c"
+// #include "exit.c"
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include "exec.h"
+
+int main(int argc, char **argv)
+{
+    char *command;
+
+    do
+    {
+        command = read_cmd();
+        print_prompt1();
+
+        if (!command)
+        {
+            exit(EXIT_SUCCESS);
+        }
+
+        if (command[0] == '\0' || strcmp(command, "\n") == 0)
+        {
+            free(command);
+            continue;
+        }
+
+        if (strcmp(command, "exit\n") == 0)
+        {
+            free(command);
+            break;
+        }
+
+        printf("%s\n", command);
+
+        free(command);
+
+    } while (1);
+
+    exit(EXIT_SUCCESS);
+}
+char *read_cmd(void)
+{
+    char buf[1024];
+    char *ptr = NULL;
+    char ptrlen = 0;
+
+    while (fgets(buf, 1024, stdin))
+    {
+        int bufferlen = strlen(buf);
+
+        if (!ptr)
+        {
+            ptr = malloc(bufferlen + 1);
+        }
+        else
+        {
+            char *ptr2 = realloc(ptr, ptrlen + bufferlen + 1);
+
+            if (ptr2)
+            {
+                ptr = ptr2;
+            }
+            else
+            {
+                free(ptr);
+                ptr = NULL;
+            }
+        }
+
+        if (!ptr)
+        {
+            fprintf(stderr, "error: failed to alloc buffer: %s\n", strerror(errno));
+            return NULL;
+        }
+
+        strcpy(ptr + ptrlen, buf);
+
+        if (buf[bufferlen - 1] == '\n')
+        {
+            if (bufferlen == 1 || buf[bufferlen - 2] != '\\')
+            {
+                return ptr;
+            }
+
+            ptr[ptrlen + bufferlen - 2] = '\0';
+            bufferlen -= 2;
+            print_prompt2();
+        }
+
+        ptrlen += bufferlen;
+    }
+
+    return ptr;
+}

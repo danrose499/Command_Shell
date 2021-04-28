36eecbfa1f0e1b3d67f726ee754f910231a0929a
Daniel Rosenthal <57165307+danrose499@users.noreply.github.com>
2021-04-28 16:55:37 -0400

    Added fork for lsDir

--- exit.c
+++ exit.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/wait.h>
 #define len 1024                                             // Default value set for length of c strings declared below 
 
 char c1[len] = "", c2[len] = "", c3[len] = "", c4[len] = ""; // Four c strings to hold the four most recent commands; Values are updated in logCommand() and printed in printCommands()                                           
@@ -23,21 +24,29 @@ int Exit() {                                                 // Exit function to
 }
 
 void logCommand(char newCommand[]){                          // Function: Called in shell.c after shell.c executes a command adding that command to the command log
-    strcpy(c1, c2); strcpy(c2, c3); strcpy(c3, c4);          // Pushes each entry down the list, overwritting the oldest entry and freeing up c4...
-    strcpy(c4, newCommand);                                  // ... and saves the newest command into c4
+    strcpy(c1, c2); strcpy(c2, c3); strcpy(c3, c4);          // Pushes each entry down the list, overwritting the oldest entry and freeing up c4
+    strcpy(c4, newCommand);                                  // Saves the newest command into c4
 }
 void printCommands(){                                        // Function: Prints a list the last 4 commands called in shell.c to the terminal 
     printf("\nPrevious four commands:\n");
     printf("%s\n%s\n%s\n%s\n\n", c4, c3, c2, c1);
 }
 void lsDir(){                                                // Function: Prints a detailed list of all content of the current directory (like ls -l)
-    char *argv[] = {"ls", "-l", 0};
-    execvp(argv[0], argv);
-    fprintf(stderr, "ls -l failed!\n");                      // Prints if execvp fails
+    int pid = fork();  
+	if(pid == 0) {                                           // Child case of fork calls ls -l
+		char *argv[] = {"ls", "-l", NULL};
+		execvp(argv[0],argv);
+		printf("EXECVP Failed\n");	                         // Prints if execvp fails
+	}
+	else {                                                   // Parent case of fork waits for child
+        wait(NULL);		
+	}
 }
 void waitForReturn(){                                        // Function: Waits for "return" input before returning
     char returnCheck = 0x00;                                 // Variable to store user input key and check for "return"
     while(returnCheck != 0x0A){                              // Wait for "return" (0x0A is ASCII for "return" key)
-        returnCheck = fgetc(stdin);                          // Get new user input until input is "return" key
+        printf("\nPress RETURN to return the control to the original shell program:\n");
+        returnCheck = getchar();                             // Get new user input until input is "return" key
     }
+    return;
 }

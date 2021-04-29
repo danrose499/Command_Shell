06ab6533768661f98f0da50e937d8c2f19858019
Daniel Rosenthal <57165307+danrose499@users.noreply.github.com>
2021-04-28 22:03:06 -0400

    Update shell.c

--- shell.c
+++ shell.c
@@ -16,14 +16,9 @@
 #include "path.c"
 #include "exit.c"
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 
-    // int cont = 1;
-    pid_t pid;
-
-    while (1) // while true
-    {
+    while(true) {
         char commandInput[1000]; // buffer size 
         printf("# ");
 
@@ -31,49 +26,41 @@ int main(int argc, char *argv[])
         // %[^\n]s : it is an edit conversion code that can be an alternative of gets(), and terminates with a newline
         scanf(" %[^\n]s", commandInput); // scanf allow us to scan input from standard in pr keyboard. it will read all characters until \n is reached
 
+        // logCommand is a user defined function in exit.c that keeps a log of the four most recently called commands in exit.c
         logCommand(commandInput);
-        // strcmp :  function used to compare string arguments character by character 
-        if (!strcmp(commandInput, "tree"))
-        {
+        // strcmp :  function used to compare string arguments character by character
+        /* User-defined commands: tree, list, path, exit */
+        if (!strcmp(commandInput, "tree")) {
             Tree();
         }
-        else if (!strcmp(commandInput, "list"))
-        {
+        else if (!strcmp(commandInput, "list")) {
             List();
         }
-        else if (!strcmp(commandInput, "path"))
-        {
+        else if (!strcmp(commandInput, "path")) {
             Path();
         }
-        else if (!strcmp(commandInput, "exit"))
-        {
-            return Exit();
+        else if (!strcmp(commandInput, "exit")) {
+            return Exit(); // Program terminates when Exit() is called
         }
-        else
-        {
-            // continue the program if not quit is pressed!
+        else { // For non user-defined commands, pass them to execvp and execute
             char *arg[100], *token, *status = NULL;
             int i = 0;
 
             // array pointer going to the next token, or NULL if there are not more tokens
-            for (token = strtok_r(commandInput, " ", &status); token != NULL; token = strtok_r(NULL, " ", &status))
-            {
+            for (token = strtok_r(commandInput, " ", &status); token != NULL; token = strtok_r(NULL, " ", &status)) {
                 arg[i] = token; // arguments store in array arg
                 i++;
             }
             arg[i] = NULL; // clear array
+            pid_t pid = fork(); // fork child process to be executed
 
-            pid = fork(); // fork child process to be executed
-
-            if (pid == 0)
-            {
+            if (pid == 0) { // Child calls execvp
                 execvp(arg[0], arg); // replaces the current process with a new one specified by the file
                 printf("File/command NOT Found\n");
                 return 0;
             }
 
-            else
-            {
+            else {
                 wait(NULL); // parent waits until child finishes
             }
         }
